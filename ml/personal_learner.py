18849cc3ba224fac100b15288c184cdcb5c34c46
yrolland <yann.rolland@iadvize.com>
2025-08-16 14:42:55 +0200

    add verbose logging for SGDClassifier initialization to improve debugging 🔍✨

--- ml/personal_learner.py
+++ ml/personal_learner.py
@@ -13,7 +13,7 @@ class PersonalLearner:
         logger.info("=" * 72)
         logger.info("[ModelInit] NEW PersonalLearner created n_features=%d classes=%s", n_features, self.classes.tolist())
         logger.info("=" * 72)
-        self.model = SGDClassifier(loss="log_loss", max_iter=1, warm_start=True)
+        self.model = SGDClassifier(loss="log_loss", max_iter=1, warm_start=True, verbose=1)
         self.scaler = StandardScaler(with_mean=True, with_std=True)
         self._is_initialized = False
 
