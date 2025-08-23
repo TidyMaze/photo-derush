4a85184884d2a915df6cb30ea8e50c615238c147
yrolland <yann.rolland@iadvize.com>
2025-08-23 22:54:04 +0200

    fix: emit images_changed only at start and end of image loading 🛠️🖼️

--- src/viewmodel.py
+++ src/viewmodel.py
@@ -77,7 +77,7 @@ class PhotoViewModel(QObject):
 
     def load_images(self):
         self.images = []
-        self.images_changed.emit(self.images)
+        self.images_changed.emit(self.images)  # Clear grid at start
         self._loader_worker = ImageLoaderWorker(self.model)
         self._loader_thread = QThread()
         self._loader_worker.moveToThread(self._loader_thread)
@@ -85,15 +85,18 @@ class PhotoViewModel(QObject):
         self._loader_worker.image_found.connect(self._on_image_found)
         self._loader_worker.thumbnail_loaded.connect(self.thumbnail_loaded)
         self._loader_worker.progress.connect(self.progress_changed)
+        self._loader_worker.finished.connect(self._on_loader_finished)
         self._loader_worker.finished.connect(self._loader_thread.quit)
         self._loader_worker.finished.connect(self._loader_worker.deleteLater)
         self._loader_thread.finished.connect(self._loader_thread.deleteLater)
         self._loader_thread.start()
 
+    def _on_loader_finished(self):
+        self.images_changed.emit(self.images.copy())
+
     @Slot(str)
     def _on_image_found(self, filename):
         self.images.append(filename)
-        self.images_changed.emit(self.images.copy())
         self.image_added.emit(filename, len(self.images) - 1)
 
     def _update_rating_tags(self):
