9dcc6b8928fb1a2d780617da264338cd9cbd7192
yrolland <yann.rolland@iadvize.com>
2025-09-01 21:35:11 +0200

    fix: correct import statement for consistency in view module 🛠️🔄

--- src/view.py
+++ src/view.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QTextEdit, QProgressBar, QGridLayout, QScrollArea, QLabel, QComboBox, QHBoxLayout, QPushButton, QLineEdit, QDialog
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QTextEdit, QProgressBar, QGridLayout, QScrollArea, QLabel, QHBoxLayout, QPushButton, QDialog
 from PySide6.QtGui import QPixmap, QImage
 from PySide6.QtCore import Qt
 import os
